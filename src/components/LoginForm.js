e573e913d32f893ce0d55095dda3d6af31fcb7b8
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2023-06-03 14:03:22 +0300

    Create LoginForm.js

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  // Login form logic here
+  render() {
+    return (
+      <div>
+        {/* Login form UI here */}
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
