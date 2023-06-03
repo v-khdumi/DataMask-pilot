b2057d9e28ca55c3f63e635d0e4be08ca1c0b7fc
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2023-06-03 14:02:37 +0300

    Create RegistrationForm.js

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+  // Registration form logic here
+  render() {
+    return (
+      <div>
+        {/* Registration form UI here */}
+      </div>
+    );
+  }
+}
+
+export default RegistrationForm;
