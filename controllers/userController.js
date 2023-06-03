eb834a33ec317251485e68f3552cd2be8080b2c3
v-khdumi <30616704+v-khdumi@users.noreply.github.com>
2023-06-03 13:56:55 +0300

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  // Registration logic here
+};
+
+exports.login = async (req, res) => {
+  // Login logic here
+};
